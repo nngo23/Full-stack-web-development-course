1af4a16a253afefcf540efddc02247ac38c5f13e
Ngo Ngoc Sang <ngocsang26889@gmail.com>
2025-10-02 02:25:43 +0300

    1.5 code

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -8,7 +8,7 @@ const Content = ({course}) => {
   return (
     <div>
       <Part part_name={course.parts[0].name} exercises={course.parts[0].exercises}/>
-      <Part part_name={course.parts[1].name} exercises={course.parts[2].exercises}/>
+      <Part part_name={course.parts[1].name} exercises={course.parts[1].exercises}/>
       <Part part_name={course.parts[2].name} exercises={course.parts[2].exercises}/>
     </div>
   )
