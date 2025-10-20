aff12313e2e898287748e904a3e48a341e52f68a
Ngo Ngoc Sang <ngocsang26889@gmail.com>
2025-10-20 13:37:12 +0300

    cleaner codes

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,13 +1,11 @@
 import {useState, useEffect} from 'react'
 
 import Filter from './components/filter'
-//import CountryForm from './components/CountryForm'
 import Countries from './components/countries'
 import CountriesServices from './services/countriess'
 
 const App = () => {
   const [countries, setCountries] = useState([]) 
-  //const [newName, setNewName] = useState('')
   const [filteredName, setFiltered] = useState('')
    
   useEffect(() => {
@@ -19,19 +17,6 @@ const App = () => {
   }, [])
 
   const countriesToShow = countries.filter(country => country.name.common.trim().toLowerCase().includes(filteredName.toLowerCase()))
-  
-
-  //const addName = (event) => {
-   // event.preventDefault()
-    //const newCountry = {name: newName}
-    //countriesServices
-   // .create(newCountry)
-   // .then(addedCountries => {
-     // setCountries(newCountry.concat(addedCountries))
-      //setNewName('')
-    //})
-  //}
-  //const handleNewName = (e) => setNewName(e.target.value)
   const handleFilteredName = (e) => setFiltered(e.target.value)
   
   return (
