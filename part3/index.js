e8c4ff75d415ae615fe54369054f4b9a3b6aa725
Ngo Ngoc Sang <ngocsang26889@gmail.com>
2025-10-22 20:58:20 +0300

    3.16 codes

--- part3/index.js
+++ part3/index.js
@@ -5,8 +5,7 @@ const app = express()
 const Person = require('./models/person')
 app.use(cors())
 app.use(express.static('dist'))
-
-app.use(express.json())
+  app.use(express.json())
 morgan.token('postPerson',request => {
   if (request.method === 'POST') {
     return JSON.stringify(request.body)
@@ -15,34 +14,53 @@ morgan.token('postPerson',request => {
 app.use(morgan(
   ':method :url :status :res[content-length] - :response-time ms :postPerson'))
 
-app.get('/api/persons', (request, response) => {
-  Person.find({}).then(person => {
-    response.json(person)
-  })
+app.get('/api/persons', (request, response, next) => {
+  Person.find({}).then(person => 
+    response.json(person))
+    .catch(next)
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const {name, number}=request.body 
   if (!name || !number) { 
     return response.status(400).json({error: 'name or number missing'})
    } 
   const person = new Person({name, number}) 
   person.save().then(saved_person =>
-    response.json(saved_person) ) }  
+    response.json(saved_person)) 
+    .catch(next)
+  }  
 ) 
 
-app.delete('/api/persons/:id', (req, res, next) => {
-  Person.findByIdAndDelete(req.params.id)
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
     .then(person => {
       if (person) {
-        res.status(204).end()
+        response.status(204).end()
       } else {
-        res.status(404).json({ error: 'person not found' })
+        response.status(404).json({ error: 'person not found' })
       }
     })
-    .catch(error => next(error))
+    .catch(next)
 })
-  
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({error: 'unknown endpoint'})
+}
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(err.message)
+  if (error.name === 'ValidationError') {
+    return response.status(400).json({error: error.message})
+  } 
+  if (error.name === 'CastError') {
+    return response.status(400).send({error:'Invalid ID format'})
+  }
+  response.status(500).json({error: 'Internal server error'})
+}
+app.use(errorHandler)
+
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
