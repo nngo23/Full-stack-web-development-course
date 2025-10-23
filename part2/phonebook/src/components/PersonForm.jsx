05b634201cd1116be9e04753a29a0fd052d01d78
Ngo Ngoc Sang <ngocsang26889@gmail.com>
2025-10-23 15:53:08 +0300

    update

--- part2/phonebook/src/components/PersonForm.jsx
+++ part2/phonebook/src/components/PersonForm.jsx
@@ -1,20 +1,31 @@
-const PersonForm = ({addName,newName,handleNewName,newNumber,handleNewNumber}) => {
-    return (
-        <form onSubmit={addName}>
-        <div>
-        name: <input 
-                value={newName} 
-                onChange={handleNewName}
-              />
-        </div>
-        <div>
-        number: <input 
-                  value={newNumber}
-                  onChange={handleNewNumber}
-              />
-        </div>
-        <div><button type="submit">add</button></div>
-      </form>
-    )
+const PersonForm = ({ addName, newName, handleNewName, newNumber, handleNewNumber }) => {
+  return (
+    <form
+      onSubmit={e => {
+        e.preventDefault()
+        console.log('🚀 FORM SUBMITTED') // add this
+        addName(e)
+      }}
+    >
+      <div>
+        name:{' '}
+        <input
+          value={newName}
+          onChange={handleNewName}
+        />
+      </div>
+      <div>
+        number:{' '}
+        <input
+          value={newNumber}
+          onChange={handleNewNumber}
+        />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  )
 }
+
 export default PersonForm
