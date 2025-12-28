82092099b902fcf4c1eaf4b413d65d5a00dcef2e
Ngo Ngoc Sang <ngocsang26889@gmail.com>
2025-12-28 10:02:06 +0200

    8.9 codes

---
+++ part8/part8-library-frontend/src/components/Books.jsx
@@ -0,0 +1,49 @@
+import { gql } from "@apollo/client";
+import { useQuery } from "@apollo/client/react";
+
+const ALL_BOOKS = gql`
+  query {
+    allBooks {
+      title
+      published
+      author
+    }
+  }
+`;
+
+const Books = (props) => {
+  if (!props.show) {
+    return null;
+  }
+
+  const result = useQuery(ALL_BOOKS);
+
+  if (result.loading) {
+    return <div>loading...</div>;
+  }
+
+  return (
+    <div>
+      <h2>books</h2>
+
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>author</th>
+            <th>published</th>
+          </tr>
+          {result.data.allBooks.map((a) => (
+            <tr key={a.id}>
+              <td>{a.title}</td>
+              <td>{a.author}</td>
+              <td>{a.published}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Books;
