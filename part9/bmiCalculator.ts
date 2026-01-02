0c9b2dedefccfd23f02e95b701826108d70d1a4e
Ngo Ngoc Sang <ngocsang26889@gmail.com>
2026-01-02 13:32:56 +0200

    9.1

---
+++ part9/bmiCalculator.ts
@@ -0,0 +1,13 @@
+const calculateBmi = (height: number, weight: number): string => {
+  const bmi = weight / ((height * height) / 10000);
+  if (bmi < 18.5) {
+    return "Underweight";
+  } else if (bmi < 25) {
+    return "Normal range";
+  } else if (bmi < 30) {
+    return "Overweight";
+  }
+  return "Obese";
+};
+
+console.log(calculateBmi(180, 74));
